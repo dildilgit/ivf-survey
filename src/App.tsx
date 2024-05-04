7e7a8d6bc3164f118bffd9da63e742629a0cbbac
Dilan Ustek <dilan.ustek@zapier.com>
2024-05-04 12:06:18 -0700

    add attemps table

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
+import { createTheme, Divider, ThemeProvider } from "@mui/material";
 import { useState } from "react";
 import "./App.css";
 import { StepSection } from "./StepSection";
+import Typography from "@mui/material/Typography";
 
 function App() {
   const [step, setStep] = useState<number>(0);
 
   return (
-    <div className="App">
-      <h1>IVF Numbers Game</h1>
-      <StepSection step={step} setStep={setStep} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Typography variant="h4" gutterBottom color="primary">
+          IVF Numbers Game
+        </Typography>
+        <div className="contentSection">
+          <Divider className="divider" sx={{ borderBottomWidth: 3 }} />
+          <div className="stepSection">
+            <StepSection step={step} setStep={setStep} />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#217074",
+      light: "#5499a4",
+      dark: "#00474d",
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: "#8B9D77",
+      light: "#B0C17A",
+      dark: "#5F7A60",
+      contrastText: "#000000",
+    },
+    divider: "#EDC5AB",
+    text: {
+      primary: "#8B9D77",
+    },
+  },
+});
 
 export default App;
--- src/StepOne.tsx
+++ src/StepOne.tsx
@@ -1,26 +1,108 @@
-import { TextField, Button, FormLabel, FormHelperText } from "@mui/material";
+import {
+  TextField,
+  Button,
+  FormLabel,
+  FormHelperText,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@mui/material";
 import { FormControl } from "@mui/base/FormControl";
 import { useState } from "react";
 
 type Data = {
   age: number | null;
-  amh: number | null;
-  fsh: number | null;
+  amh: string;
+  fsh: string;
   afc: number | null;
   attempts: number | null;
+  ivfData: IVFAttemptData[];
 };
+
+type IVFAttemptData = {
+  folliclesAtRetrieval: number | null;
+  eggsRetrieved: number | null;
+  fertilizedOnDay1: number | null;
+  day3EmbryosTransferred: number | null;
+  blasts: number | null;
+  pgtNormalEmbryos: number | null;
+  day5PlusEmbryosTransferred: number | null;
+};
+
+const headers: { label: string; key: keyof IVFAttemptData }[] = [
+  { label: "Follicles at Retrieval", key: "folliclesAtRetrieval" },
+  { label: "Eggs Retrieved", key: "eggsRetrieved" },
+  { label: "Fertilized on Day 1", key: "fertilizedOnDay1" },
+  { label: "Day 3 Embryos Transferred", key: "day3EmbryosTransferred" },
+  { label: "Blasts", key: "blasts" },
+  { label: "PGT Normal Embryos", key: "pgtNormalEmbryos" },
+  { label: "Day 5+ Embryos Transferred", key: "day5PlusEmbryosTransferred" },
+];
 
 export function Step1({ setStep }: { setStep: (step: number) => void }) {
   const [data, setData] = useState<Data>({
     age: null,
-    amh: null,
-    fsh: null,
+    amh: "",
+    fsh: "",
     afc: null,
     attempts: null,
+    ivfData: [],
   });
 
   console.log(data);
 
+  const handleInputChange = (
+    index: number,
+    field: keyof IVFAttemptData,
+    value: string
+  ): void => {
+    const numericValue = value === "" ? null : Number(value);
+    setData((prevData) => {
+      const newIVFData = [...prevData.ivfData];
+      if (!newIVFData[index]) {
+        newIVFData[index] = {
+          folliclesAtRetrieval: null,
+          eggsRetrieved: null,
+          fertilizedOnDay1: null,
+          day3EmbryosTransferred: null,
+          blasts: null,
+          pgtNormalEmbryos: null,
+          day5PlusEmbryosTransferred: null,
+        };
+      }
+      newIVFData[index][field] = numericValue;
+      return { ...prevData, ivfData: newIVFData };
+    });
+  };
+  const renderInputRow = (index: number) => (
+    <TableRow key={index}>
+      <TableCell>{index + 1}</TableCell>
+      {headers.map((header) => {
+        const key = header.key;
+        const attemptData = data.ivfData[index];
+        const value =
+          attemptData && attemptData[key] !== null
+            ? attemptData[key]?.toString()
+            : "";
+
+        return (
+          <TableCell key={key}>
+            <TextField
+              variant="outlined"
+              size="small"
+              type="number"
+              value={value}
+              onChange={(e) => handleInputChange(index, key, e.target.value)}
+            />
+          </TableCell>
+        );
+      })}
+    </TableRow>
+  );
   return (
     <div>
       <form
@@ -63,9 +145,10 @@ export function Step1({ setStep }: { setStep: (step: number) => void }) {
             variant="standard"
             required
             value={data.amh}
-            onChange={(event) =>
-              setData({ ...data, amh: parseInt(event.target.value) })
-            }
+            onChange={(event) => setData({ ...data, amh: event.target.value })}
+            inputProps={{
+              step: "0.01", // Allows decimal inputs to two decimal places
+            }}
           />
           <FormHelperText sx={{ fontSize: "0.8em" }}>
             You can{" "}
@@ -88,15 +171,16 @@ export function Step1({ setStep }: { setStep: (step: number) => void }) {
             variant="standard"
             required
             value={data.fsh}
-            onChange={(event) =>
-              setData({ ...data, fsh: parseInt(event.target.value) })
-            }
+            onChange={(event) => setData({ ...data, fsh: event.target.value })}
+            inputProps={{
+              step: "0.01", // Allows decimal inputs to two decimal places
+            }}
           />
         </FormControl>
         <FormControl className="question">
           <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-            What is your Antral Follicular Count (AFC)? You can provide an
-            average since it is not always the same.
+            What is your Antral Follicular Count (AFC)? You can provide the
+            number that you see the most (mode) since it is not always the same.
           </FormLabel>
           <TextField
             id="standard-number"
@@ -116,8 +200,8 @@ export function Step1({ setStep }: { setStep: (step: number) => void }) {
         <FormControl className="question">
           <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
             How many IVF attempts have you had so far that you haven't reported
-            to the sheet? If you've previously reported an attempt, please only
-            submit new ones.
+            to the sheet? <br /> If you've previously reported an attempt,
+            please only submit new ones.
           </FormLabel>
           <TextField
             id="standard-number"
@@ -135,8 +219,30 @@ export function Step1({ setStep }: { setStep: (step: number) => void }) {
           />
         </FormControl>
 
+        {data.attempts !== null && (
+          <TableContainer component={Paper}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Attempt #</TableCell>
+                  {headers.map((header) => (
+                    <TableCell key={header.key}>{header.label}</TableCell> // Correct use of label for display
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {Array.from({ length: data.attempts || 0 }, (_, index) =>
+                  renderInputRow(index)
+                )}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+
         <div className="nextButton">
-          <Button variant="contained">Next</Button>
+          <Button variant="contained" color="primary">
+            Next
+          </Button>
         </div>
       </form>
     </div>
