7c17c67ffa2e347837e81cb3a974ad04d10e15e9
Dilan Ustek <dilan.ustek@zapier.com>
2024-05-26 17:14:27 -0700

    accordion

--- src/StepTwo.tsx
+++ src/StepTwo.tsx
@@ -6,9 +6,14 @@ import {
   Button,
   FormLabel,
   FormControlLabel,
+  AccordionSummary,
 } from "@mui/material";
 import { FormControl } from "@mui/base/FormControl";
 import { IVFAttemptData, SurveyData } from "./StepSection";
+import Accordion from "@mui/material/Accordion";
+import { useState } from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AccordionDetails from "@mui/material/AccordionDetails";
 
 export const generateNewIVFData = (index: number): IVFAttemptData => ({
   attemptNumber: index,
@@ -33,9 +38,9 @@ export function Step2({
   data: SurveyData;
   setData: React.Dispatch<React.SetStateAction<SurveyData>>;
 }) {
-  let isFormValid = true;
-  console.log("isFormValid = ", isFormValid);
+  const [expanded, setExpanded] = useState<string | false>(false);
 
+  let isFormValid = true;
   Object.values(data.ivfData).map((data) => {
     const isFertilizationValid =
       !!data.fertilizedOnDay1 && !!data.eggsRetrieved
@@ -116,228 +121,265 @@ export function Step2({
         data.day5PlusEmbryosTransferred > data.blasts);
 
     return (
-      <Paper className="attemptSection" key={index}>
-        <Typography className="attemptTitle" variant="h5">
-          {" "}
-          Attempt # {index + 1}{" "}
-        </Typography>
-        <div className="attemptQuestions">
-          <FormControl className="question">
-            <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-              # Follicles at Retrieval
-            </FormLabel>
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="standard"
-              required
-              value={data.folliclesAtRetrieval}
-              onChange={(event) =>
-                handleInputChange(
-                  index,
-                  "folliclesAtRetrieval",
-                  event.target.value
-                )
-              }
-            />
-          </FormControl>
-          <FormControl className="question">
-            <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-              # Eggs Retrieved
-            </FormLabel>
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="standard"
-              value={data.eggsRetrieved}
-              onChange={(event) =>
-                handleInputChange(index, "eggsRetrieved", event.target.value)
-              }
-            />
-          </FormControl>
+      <div className="attemptSection">
+        <Accordion
+          expanded={expanded === `panel${index}`}
+          onChange={handleAccordionChange(`panel${index}`)}
+        >
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <Typography
+              sx={{ width: "70%", flexShrink: 0 }}
+              className="attemptTitle"
+            >
+              {" "}
+              Attempt # {index + 1}{" "}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div className="attemptQuestions">
+              <FormControl className="question">
+                <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
+                  # Follicles at Retrieval
+                </FormLabel>
+                <TextField
+                  id="standard-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="standard"
+                  required
+                  value={data.folliclesAtRetrieval}
+                  onChange={(event) =>
+                    handleInputChange(
+                      index,
+                      "folliclesAtRetrieval",
+                      event.target.value
+                    )
+                  }
+                />
+              </FormControl>
+              <FormControl className="question">
+                <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
+                  # Eggs Retrieved
+                </FormLabel>
+                <TextField
+                  id="standard-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="standard"
+                  value={data.eggsRetrieved}
+                  onChange={(event) =>
+                    handleInputChange(
+                      index,
+                      "eggsRetrieved",
+                      event.target.value
+                    )
+                  }
+                />
+              </FormControl>
 
-          <FormControl className="question">
-            <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-              # Fertilized on Day 1
-            </FormLabel>
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              error={isFertilizationValid}
-              helperText={
-                isFertilizationValid
-                  ? "Number fertilized must be less than or equal to the number of eggs"
-                  : ""
-              }
-              variant="standard"
-              value={data.fertilizedOnDay1}
-              onChange={(event) =>
-                handleInputChange(index, "fertilizedOnDay1", event.target.value)
-              }
-            />
-          </FormControl>
-          <FormControl className="question">
-            <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-              # Day 3 Embryos Transferred
-            </FormLabel>
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="standard"
-              value={data.day3EmbryosTransferred}
-              error={isday3TransferValid}
-              helperText={
-                isday3TransferValid
-                  ? "Number transferred must be less than or equal to the number of eggs"
-                  : ""
-              }
-              onChange={(event) =>
-                handleInputChange(
-                  index,
-                  "day3EmbryosTransferred",
-                  event.target.value
-                )
-              }
-            />
-          </FormControl>
-          <FormControl className="question">
-            <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-              # Blastocysts (Day 5+)
-            </FormLabel>
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="standard"
-              value={data.blasts}
-              error={
-                !!data.blasts &&
-                !!data.eggsRetrieved &&
-                data.blasts > data.eggsRetrieved
-              }
-              helperText={
-                !!data.blasts &&
-                !!data.eggsRetrieved &&
-                data.blasts > data.eggsRetrieved
-                  ? "Number of blastocysts can not be more than the number of eggs and there shouldn't be more PGT normal embryos than there are blasts"
-                  : ""
-              }
-              onChange={(event) =>
-                handleInputChange(index, "blasts", event.target.value)
-              }
-            />
-          </FormControl>
-          <FormControl className="question">
-            <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-              # PGT Normal Embryos
-            </FormLabel>
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="standard"
-              value={data.pgtNormalEmbryos}
-              error={isPgtNormalValid}
-              helperText={
-                isPgtNormalValid
-                  ? "Number of blastocysts must be less than or equal to the number of eggs"
-                  : ""
-              }
-              onChange={(event) =>
-                handleInputChange(index, "pgtNormalEmbryos", event.target.value)
-              }
-            />
-          </FormControl>
-          <FormControl className="question">
-            <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
-              # Day 5+ Embryos Transferred
-            </FormLabel>
-            <TextField
-              id="standard-number"
-              label="Number"
-              type="number"
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant="standard"
-              value={data.day5PlusEmbryosTransferred}
-              error={isDay5TransferValid}
-              helperText={
-                isDay5TransferValid
-                  ? "Number of day 5 embryos cannot be more than the number of eggs or blasts"
-                  : ""
-              }
-              onChange={(event) =>
-                handleInputChange(
-                  index,
-                  "day5PlusEmbryosTransferred",
-                  event.target.value
-                )
-              }
-            />
-          </FormControl>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={data.isCancelled}
-                onChange={(event) =>
-                  handleInputChange(index, "isCancelled", event.target.checked)
+              <FormControl className="question">
+                <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
+                  # Fertilized on Day 1
+                </FormLabel>
+                <TextField
+                  id="standard-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  error={isFertilizationValid}
+                  helperText={
+                    isFertilizationValid
+                      ? "Number fertilized must be less than or equal to the number of eggs"
+                      : ""
+                  }
+                  variant="standard"
+                  value={data.fertilizedOnDay1}
+                  onChange={(event) =>
+                    handleInputChange(
+                      index,
+                      "fertilizedOnDay1",
+                      event.target.value
+                    )
+                  }
+                />
+              </FormControl>
+              <FormControl className="question">
+                <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
+                  # Day 3 Embryos Transferred
+                </FormLabel>
+                <TextField
+                  id="standard-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="standard"
+                  value={data.day3EmbryosTransferred}
+                  error={isday3TransferValid}
+                  helperText={
+                    isday3TransferValid
+                      ? "Number transferred must be less than or equal to the number of eggs"
+                      : ""
+                  }
+                  onChange={(event) =>
+                    handleInputChange(
+                      index,
+                      "day3EmbryosTransferred",
+                      event.target.value
+                    )
+                  }
+                />
+              </FormControl>
+              <FormControl className="question">
+                <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
+                  # Blastocysts (Day 5+)
+                </FormLabel>
+                <TextField
+                  id="standard-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="standard"
+                  value={data.blasts}
+                  error={
+                    !!data.blasts &&
+                    !!data.eggsRetrieved &&
+                    data.blasts > data.eggsRetrieved
+                  }
+                  helperText={
+                    !!data.blasts &&
+                    !!data.eggsRetrieved &&
+                    data.blasts > data.eggsRetrieved
+                      ? "Number of blastocysts can not be more than the number of eggs and there shouldn't be more PGT normal embryos than there are blasts"
+                      : ""
+                  }
+                  onChange={(event) =>
+                    handleInputChange(index, "blasts", event.target.value)
+                  }
+                />
+              </FormControl>
+              <FormControl className="question">
+                <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
+                  # PGT Normal Embryos
+                </FormLabel>
+                <TextField
+                  id="standard-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="standard"
+                  value={data.pgtNormalEmbryos}
+                  error={isPgtNormalValid}
+                  helperText={
+                    isPgtNormalValid
+                      ? "Number of blastocysts must be less than or equal to the number of eggs"
+                      : ""
+                  }
+                  onChange={(event) =>
+                    handleInputChange(
+                      index,
+                      "pgtNormalEmbryos",
+                      event.target.value
+                    )
+                  }
+                />
+              </FormControl>
+              <FormControl className="question">
+                <FormLabel sx={{ fontSize: "1.1em" }} className="questionLabel">
+                  # Day 5+ Embryos Transferred
+                </FormLabel>
+                <TextField
+                  id="standard-number"
+                  label="Number"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="standard"
+                  value={data.day5PlusEmbryosTransferred}
+                  error={isDay5TransferValid}
+                  helperText={
+                    isDay5TransferValid
+                      ? "Number of day 5 embryos cannot be more than the number of eggs or blasts"
+                      : ""
+                  }
+                  onChange={(event) =>
+                    handleInputChange(
+                      index,
+                      "day5PlusEmbryosTransferred",
+                      event.target.value
+                    )
+                  }
+                />
+              </FormControl>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={data.isCancelled}
+                    onChange={(event) =>
+                      handleInputChange(
+                        index,
+                        "isCancelled",
+                        event.target.checked
+                      )
+                    }
+                    name="cancelled"
+                  />
                 }
-                name="cancelled"
+                label="Was cancelled?"
               />
-            }
-            label="Was cancelled?"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={data.isDonor}
-                onChange={(event) =>
-                  handleInputChange(index, "isDonor", event.target.checked)
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={data.isDonor}
+                    onChange={(event) =>
+                      handleInputChange(index, "isDonor", event.target.checked)
+                    }
+                    name="donor"
+                  />
                 }
-                name="cancelled"
+                label="Donor eggs?"
               />
-            }
-            label="Donor eggs?"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={data.isICSI}
-                onChange={(event) =>
-                  handleInputChange(index, "isICSI", event.target.checked)
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={data.isICSI}
+                    onChange={(event) =>
+                      handleInputChange(index, "isICSI", event.target.checked)
+                    }
+                    name="icsi"
+                  />
                 }
-                name="cancelled"
+                label="ICSI fertilization?"
               />
-            }
-            label="ICSI fertilization?"
-          />
-        </div>
-      </Paper>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+      </div>
     );
   };
+
+  const handleAccordionChange =
+    (panel: string) => (event: React.SyntheticEvent, isExpanded: boolean) => {
+      setExpanded(isExpanded ? panel : false);
+    };
 
   return (
     <form className="form" onSubmit={() => setStep(3)}>
